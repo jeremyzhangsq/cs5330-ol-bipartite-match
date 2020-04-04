06da3187aa94c7ae98726abb7b5729051e212dbd
Your Name <you@example.com>
2020-04-04 14:39:21 +0800

    add greedy

--- baseline.py
+++ baseline.py
@@ -1,10 +1,17 @@
+import random
 
 
 def greedy(vertex, neighbors, graph, match):
-    pass
+    if len(neighbors) != 0:
+        selectedVertex = random.choice(neighbors)
+        neighbors.remove(selectedVertex)
+        match_pair = [vertex, selectedVertex]
+        match.append(match_pair)
+
 
 def rank(vertex, neighbors, graph, match):
     pass
+
 
 def offline(vertex, neighbors, graph, match):
     pass
