17d4855a247624dc484d08055387e080115dbcc1
caicre <caicre@163.com>
2020-04-21 11:51:01 +0800

    add log

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import baseline
 import alg1
 import alg2
 import alg3
+import time
 
 import logging
 # Create and configure logger
@@ -50,7 +51,7 @@ def is_match(match):
 if __name__ == '__main__':
     fid = int(sys.argv[1])
     aid = int(sys.argv[2])
-    files = ["revolution.txt","actor-movie.txt", "github.txt", "youtube.txt"]
+    files = ["revolution.txt","actor-movie.txt", "github.txt", "youtube.txt", "actor_movie_sample.txt"]
     algs = ["GREEDY", "RANK", "OFFLINE", "ALG2", "ALG3"]
     endreg = "\r\n"
     delimiter = " "
@@ -59,8 +60,12 @@ if __name__ == '__main__':
     read_network("./dataset/" + files[fid])
 
     if algs[aid] == "OFFLINE":
+        print('processing alg: ' + str(algs[aid]))
         baseline.offline()
+        offline_num_match = len(match)
+        print('num_match: %d'%(offline_num_match))
     else:
+        print('shuffling data')
         # simulate the general vertex arrival: the vertex comes with edges to previous neighbors
         stream = list(graph.keys())  # random vertex streaming
         # fix seed here
@@ -68,10 +73,22 @@ if __name__ == '__main__':
         random.shuffle(stream)
         arrived = []  # already arrived vertex
 
+        print('processing alg: ' + str(algs[aid]))
+        data_len = len(stream)
+        print_len = int(len(stream)/20)
+        i = 0
+        
+        start_time = time.time()
         if algs[aid] == "ALG3":
             alg3.main_alg3(graph, stream, k=1)
         else:
             while len(stream) != 0:
+                
+                if i%print_len == 0:
+                    print('  processing stream: %d/%d time: %f'%(i, data_len, time.time()-start_time))
+                    start_time = time.time()
+                i += 1
+
                 vertex = stream.pop()  # get next vertex
                 # get previous neighbors of current vertex
                 neighbors = list(set(graph[vertex]).intersection(arrived))
@@ -89,4 +106,14 @@ if __name__ == '__main__':
 
     assert is_match(match)
     # for debug
-    logger.debug("#Matched: %d tuples: %s" % (len(match), match))
+    # logger.debug("#Matched: %d tuples: %s" % (len(match), match))
+
+    num_match = len(match)
+    if algs[aid] != "OFFLINE":
+        print('processing alg: OFFLINE')
+        match.clear()
+        baseline.offline()
+        offline_num_match = len(match)
+        # logger.debug("#Matched: %d tuples: %s" % (len(match), match))
+        print('num_match: %d'%(offline_num_match))
+        print('competitive ratio: %f'%(num_match/offline_num_match))
